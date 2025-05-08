4de5aaa90013f3c12737775576b291c186bbbb3a
sndp bag <sndpbagg@gmail.com>
2025-05-08 13:45:06 +0530

    added next problem and their solution

--- Soulations.js
+++ Soulations.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 function formatString(input, toUpper) {
     if (toUpper === false) {
         return input.toLowerCase();
@@ -28,3 +43,31 @@ var result1 = concatenateArrays(["a", "b"], ["c"]);
 var result2 = concatenateArrays([1, 2], [3, 4], [5]);
 console.log(result1);
 console.log(result2);
+//  Create a Vehicle class with private make and year properties and a getInfo() method.
+//Create a Car class extending Vehicle, adding a private model property and a getModel() method.
+var Vehicle = /** @class */ (function () {
+    function Vehicle(make, year) {
+        this.make = make;
+        this.year = year;
+    }
+    Vehicle.prototype.getInfo = function () {
+        return "Make: ".concat(this.make, ", Year: ").concat(this.year);
+    };
+    return Vehicle;
+}());
+var Car = /** @class */ (function (_super) {
+    __extends(Car, _super);
+    function Car(make, year, model) {
+        var _this = _super.call(this, make, year) || this;
+        _this.model = model;
+        return _this;
+    }
+    Car.prototype.getModel = function () {
+        return "Model: ".concat(this.model);
+    };
+    return Car;
+}(Vehicle));
+// Example usage:
+var myCar = new Car("Toyota", 2020, "Corolla");
+console.log(myCar.getInfo());
+console.log(myCar.getModel());
--- Soulations.ts
+++ Soulations.ts
@@ -34,3 +34,39 @@ function concatenateArrays<T>(...arrays: T[][]): T[] {
  const result2 = concatenateArrays([1, 2], [3, 4], [5]);
  console.log(result1);
  console.log(result2);
+
+//  Create a Vehicle class with private make and year properties and a getInfo() method.
+//Create a Car class extending Vehicle, adding a private model property and a getModel() method.
+
+class Vehicle {
+    private make: string;
+    private year: number;
+  
+    constructor(make: string, year: number) {
+      this.make = make;
+      this.year = year;
+    }
+  
+    public getInfo(): string {
+      return `Make: ${this.make}, Year: ${this.year}`;
+    }
+  }
+  
+  class Car extends Vehicle {
+    private model: string;
+  
+    constructor(make: string, year: number, model: string) {
+      super(make, year);
+      this.model = model;
+    }
+  
+    public getModel(): string {
+      return `Model: ${this.model}`;
+    }
+  }
+  
+  // Example usage:
+  const myCar = new Car("Toyota", 2020, "Corolla");
+  console.log(myCar.getInfo());    
+  console.log(myCar.getModel());   
+  
